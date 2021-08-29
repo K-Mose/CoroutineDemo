695fd7ced068dac296f9e1629a7b1cedb95a73a5
user <km_413@naver.com>
2021-08-29 20:26:49 +0900

    Changing Thread

--- app/src/main/java/com/example/coroutinedemo/MainActivity.kt
+++ app/src/main/java/com/example/coroutinedemo/MainActivity.kt
@@ -9,6 +9,7 @@ import com.example.coroutinedemo.databinding.ActivityMainBinding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -20,8 +21,6 @@ class MainActivity : AppCompatActivity() {
 
         binding.apply {
             btnClick.setOnClickListener {
-
-                Toast.makeText(baseContext, "Job Finished", Toast.LENGTH_SHORT).show()
                 tvCount.text = (++btnCount).toString()
             }
             btnDownload.setOnClickListener {
@@ -32,19 +31,26 @@ class MainActivity : AppCompatActivity() {
                     UI thread가 아닌 곳에서 UI 호출을 시도하면 "Can’t create handler inside thread that has not called Looper.prepare()" 발생,
                     Looper - Message loop를 실행시키기 위해 사용됨.
                     */
-                    Looper.prepare()
+
                     download()
+                    Looper.prepare()
+                    Toast.makeText(baseContext, "Job Finished", Toast.LENGTH_SHORT).show()
                     Looper.loop()
+
                 }
                 // download() // It will start at main thread
             }
         }
     }
 
-    private fun download() {
-        for ( i in 0 .. 20000) {
+    private suspend fun download() {
+        for ( i in 0 .. 1000) {
             Log.e("TAG", "Downloading user $i in ${Thread.currentThread().name}")
+            // Coroutine은 withContext() 함수로 쉽게 쓰레드 변경을 할 수 있다.
+            withContext(Dispatchers.Main) {
+                // UI에 text를 사용하기 위해 쓰레드 변경
+                binding.tvUserMessage.text = "Downloading user $i in ${Thread.currentThread().name}"
+            }
         }
-        Toast.makeText(baseContext, "Job Finished", Toast.LENGTH_SHORT).show()
     }
 }
